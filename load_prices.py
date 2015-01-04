b13f0c51137ff1c54fe179e40d1fe2d1f8728054
Nicholas Reichel <reich384@umn.edu>
2015-01-04 05:35:15 -0600

    COmpleted load percent changes

--- load_prices.py
+++ load_prices.py
@@ -4,11 +4,17 @@ con = lite.connect('./mldata.db')
 cur = con.cursor()
 
 cur.execute('SELECT Ticker from screens group by Ticker')
-    
+
 data = cur.fetchall()
+try:
+    cur.execute('DROP table price_data')
+except:
+    pass
+    
+
 for ticker in data:
     ticker = ticker[0]
-    df = read_csv('http://real-chart.finance.yahoo.com/table.csv?s={0}&d=11&e=23&f=2014&g=d&a=0&b=2&c=2013&ignore=.csv'.format(ticker))
+    print ticker
+    df = read_csv('http://real-chart.finance.yahoo.com/table.csv?s={0}&d=11&e=31&f=2014&g=d&a=0&b=2&c=2013&ignore=.csv'.format(ticker))
     df['Ticker'] = ticker
-    print df
     df.to_sql('price_data', con, if_exists='append')
